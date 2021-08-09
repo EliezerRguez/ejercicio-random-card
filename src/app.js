a46f1602cabee6d3ffb5edc8e3f5eb552d03d1df
EliezerRguez <eliezer.rguez93@gmail.com>
2021-08-09 16:02:40 +0000

    aparicion suits

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
 };
+
+function generateRandomNumber() {
+  let number = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumber = Math.floor(Math.random() * number.length);
+  return number[indexNumber];
+}
+
+function generateRandomSuit() {
+  let suit = ["spade", "club", "heart", "diamond"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+}
